065f055d4943fe1310c9bbdec6c5d2624a7815e0
Kurtmj93 <kurtmj93@gmail.com>
2023-02-15 09:28:40 -0500

    implement writeFile for css and index

--- index.js
+++ index.js
@@ -9,13 +9,22 @@ const fs = require('fs'); // allows writing to file system
 
 const questions = [];
 
+function writeFiles(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>{
+        if (err) {
+            console.log(err);
+        } // simple err callback
+    });
+}
+
 // Function to initialize app
 function init() {
     //inquirer package makes this function simple & elegant
     inquirer
         .prompt(questions)
         .then((answers) => {
-
+            writeFiles('style.css', generateCSS(answers));
+            writeFiles('index.html', generateHTML(answers));
         });
 };
 
